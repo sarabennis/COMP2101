e7f407272c3c6a558fbcff1d8f18321a0ff498ea
sarabennis <zarah522001@gmail.com>
2020-01-24 13:47:04 -0800

    second lab files

--- bash/getpics.sh
+++ bash/getpics.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-#
 # this script puts some picture files into a Pictures directory in our home directory
 # it does some error testing
 # it summarizes the Pictures directory when it is done
-#
-
 # Task 1: Improve this script to also retrieve and install the files kept in the https://zonzorp.net/pics.tgz tarfile
 #   - use the same kind of testing that is already in the script to only download the tarfile if you don't already have it and  to make sure the download and extraction commands work, then delete the local copy of the tarfile if the extraction was successful
 
+
+##########################################################################################################################
 # make a Pictures directory if we don't have one - assumes we have a home directory
 test -d ~/Pictures || mkdir ~/Pictures
 
@@ -17,23 +16,15 @@ test -f ~/Pictures/pics.zip ||wget -q -O ~/Pictures/pics.zip http://zonzorp.net/
 # unpack the downloaded zipfile and tarfile if it is there, then delete the local copy of the zipfile
 test -f ~/Pictures/pics.zip && unzip -d ~/Pictures -o -q ~/Pictures/pics.zip && rm ~/Pictures/pics.zip
 
-
+################################################################################################################################
 
 #modify and install the files as gzipped tarfile
 test -f ~/Pictures/pics.tgz || wget -q -O ~/Pictures/pics.tgz http://zonzorp.net/pics.tgz
 
-# uzip the tar file
+# uzip the tar file and delete the local copy of tar file
 test -f ~/Pictures/pics.tgz && tar -C ~/Pictures -xzf ~/Pictures/pics.tgz && rm ~/Pictures/pics.tgz
 
-
-
-
-
-
-
-
-
-
+#################################################################################################################################
 
 
 # Make a report on what we have in the Pictures directory
@@ -41,5 +32,4 @@ test -d ~/Pictures && cat <<EOF
 Found $(find ~/Pictures -type f|wc -l) files in the Pictures directory.
 The Pictures directory uses $(du -sh ~/Pictures|awk '{print $1}') space on the disk.
 EOF
-#######################################################################################3
-#task
+########################################################################################################################################
