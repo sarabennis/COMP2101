67976d6dd4ca18f53af98f8e93601cf504b4f0c5
sarabennis <zarah522001@gmail.com>
2020-01-28 11:32:10 -0800

    second lab files

--- bash/diceroller.sh
+++ bash/diceroller.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-#
 # this script rolls a six-sided die
-#
-
 # Task: Improve this script by making it roll as many dice as there are letters in your first name, instead of just one
-
 # roll the dice and display the result
 
 
