114a93c19fe5268534991e014a937855def4a98a
sarabennis <zarah522001@gmail.com>
2020-01-28 15:58:43 -0800

    second lab 2 files

--- bash/diceroller.sh
+++ bash/diceroller.sh
@@ -12,6 +12,12 @@ Rolling...
 $(( RANDOM % 6 + 1)) rolled
 "
 #################################################################################################
+echo "rolling dice 4 times since there is 4 letter in my first name"
+echo "rolling........
+($(( RANDOM % 6 + 1))-S,$(( RANDOM % 6 + 1))-A ,$(( RANDOM % 6 + 1))-R ,$(( RANDOM % 6 + 1))-A)rolled
+"
+
+################################################################################################
 
 echo "rolling a pair of 5 six sided die"
 echo "
