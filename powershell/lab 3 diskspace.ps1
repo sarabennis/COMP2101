fe01b98f38d24f229cfefa5b344992db858b2624
sarabennis <zarah522001@gmail.com>
2020-03-25 18:50:28 -0400

    lab 3 files

---
+++ powershell/lab 3 diskspace.ps1
@@ -0,0 +1,7 @@
+﻿gwmi -class win32_logicaldisk |
+where-object size -gt 0 |
+format-table -autosize DeviceID,
+@{n="Size(GB)"; e={$_.size/1gb -as [int]}},
+@{n="Free(GB)"; e={$_.freespace/1gb -as [int]}},
+@{n="% Free"; e={100*$_.freespace/$_.size -as [int]}},
+ProviderName
---
+++ powershell/lab 3 software.ps1
@@ -0,0 +1,3 @@
+﻿gwmi -class win32_product -filter "vendor!='Microsoft Corporation'"|
+sort InstallDate|
+ft Name,Vendor, Version, InstallDate
